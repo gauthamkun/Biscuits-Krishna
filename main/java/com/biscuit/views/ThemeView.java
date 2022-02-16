d9e48a57e8bb6da171ebefee63873517a9e15a36
Abhishek Mohabe <abhishek.m28@gmail.com>
2022-02-16 16:32:51 -0700

    Added the Theme.java and ThemeView.java

---
+++ main/java/com/biscuit/views/ThemeView.java
@@ -0,0 +1,104 @@
+package com.biscuit.views;
+
+import com.biscuit.commands.epic.AddEpicToBacklog;
+import com.biscuit.commands.help.BacklogHelp;
+import com.biscuit.commands.userStory.ListUserStories;
+import com.biscuit.models.Epic;
+import com.biscuit.models.UserStory;
+import com.biscuit.models.services.Finder.UserStories;
+import jline.console.completer.Completer;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ThemeView extends View {
+
+    Epic epic = null;
+
+
+    public ThemeView(View previousView, Epic epic) {
+        super(previousView, "Theme");
+        this.epic = epic;
+    }
+
+    @Override
+    void addSpecificCompleters(List<Completer> completers) {
+
+    }
+
+    @Override
+    boolean executeCommand(String[] words) throws IOException {
+        if (words.length == 1) {
+            return execute1Keyword(words);
+        } else if (words.length == 2) {
+            return execute2Keyword(words);
+        } else if (words.length == 4) {
+            return execute4Keyword(words);
+        }
+
+        return false;
+    }
+
+
+    private boolean execute4Keyword(String[] words) throws IOException {
+        if (words[0].equals("list")) {
+            if (words[1].equals("user_stories")) {
+                if (words[2].equals("filter")) {
+                    (new ListUserStories(epic, "Theme(User Stories)", true, words[3], false, "")).execute();
+                    return true;
+                } else if (words[2].equals("sort")) {
+                    (new ListUserStories(epic, "Theme(User Stories)", false, "", true, words[3])).execute();
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    private boolean execute2Keyword(String[] words) throws IOException {
+        if (words[0].equals("add")) {
+            if (words[1].equals("user_story")) {
+                (new AddEpicToBacklog(reader, this.epic.project)).execute();
+                resetCompleters();
+
+                return true;
+            }
+
+        } else if (words[0].equals("list")) {
+            if (words[1].equals("user_stories")) {
+                (new ListUserStories(epic, "Backlog (User Stories)")).execute();
+                return true;
+            }
+        } else if (words[0].equals("go_to")) {
+            if (UserStories.getAllNames(epic).contains(words[1])) {
+                UserStory us = UserStories.find(epic, words[1]);
+                if (us == null) {
+                    return false;
+                }
+
+                us.project = epic.project;
+
+                UserStroryView usv = new UserStroryView(this, us);
+                usv.view();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private boolean execute1Keyword(String[] words) throws IOException {
+        if (words[0].equals("user_stories")) {
+            (new ListUserStories(epic, "Backlog (User Stories)")).execute();
+            return true;
+        } else if (words[0].equals("help")) {
+            return (new BacklogHelp()).execute();
+        }
+
+        return false;
+    }
+
+}
